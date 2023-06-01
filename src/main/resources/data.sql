7cb5b8f273a3b222bd5d83d3648516d8159f9ddf
Martín <magadiflo@gmail.com>
2023-06-01 11:32:03 -0500

    Cap. 7 configurando autorización - Limpiando proyecto

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO usuarios(usuario, pass, activo, creacion) VALUES('admin', '12345', 1, '2020-05-14');
-INSERT IGNORE INTO usuarios(usuario, pass, activo, creacion) VALUES('martin', '12345', 1, '2020-05-14');
-
-INSERT IGNORE INTO autoridades(usuario, autoridad, creacion) VALUES('admin', 'leer', '2020-05-14');
-INSERT IGNORE INTO autoridades(usuario, autoridad, creacion) VALUES('admin', 'escribir', '2020-05-14');
-INSERT IGNORE INTO autoridades(usuario, autoridad, creacion) VALUES('martin', 'leer', '2020-05-14');
