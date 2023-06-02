1145cca2a5810a20ed802bba6aa9cef7b94d22b8
Martín <magadiflo@gmail.com>
2023-06-02 18:57:00 -0500

    Agregar un filtro antes de uno existente en la cadena

--- src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
@@ -1,17 +1,17 @@
 package com.magadiflo.book.security.app.config;
 
+import com.magadiflo.book.security.app.filters.RequestValidationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .regexMatchers(".*/(us|uk|ca)+/(en|fr).*").authenticated()
-                .anyRequest().hasAuthority("premium");
+        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests().anyRequest().permitAll();
     }
 }
