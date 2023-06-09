0cfe03de43371da7c177d09d3d558b3f4213e857
Martín <magadiflo@gmail.com>
2023-06-09 17:03:38 -0500

    Implementando ClientRegistrationRepository

--- src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
@@ -1,10 +1,13 @@
 package com.magadiflo.book.security.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
@@ -13,6 +16,12 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.oauth2Login();
 
         http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration clientRegistration = this.clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientRegistration);
     }
 
     private ClientRegistration clientRegistration() {
