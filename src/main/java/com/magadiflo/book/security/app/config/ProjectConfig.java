a56562d87366e03311dac404b88792b60bd1a196
Martín <magadiflo@gmail.com>
2023-06-09 13:43:02 -0500

    Implementando ClientRegistration

--- src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
@@ -3,6 +3,8 @@ package com.magadiflo.book.security.app.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
@@ -11,5 +13,12 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.oauth2Login();
 
         http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("7efb15ac1cdde34b0901")
+                .clientSecret("bccd7f002c96cae30645159d67d8d444bf16fb6c")
+                .build();
     }
 }
