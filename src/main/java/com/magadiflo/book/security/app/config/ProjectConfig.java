f20069285cedb4d41d50a868238d6586f5f8ed80
Martín <magadiflo@gmail.com>
2023-06-09 18:43:32 -0500

    Probando la aplicación

--- src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
+++ src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
@@ -1,13 +1,8 @@
 package com.magadiflo.book.security.app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
@@ -16,18 +11,5 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.oauth2Login();
 
         http.authorizeRequests().anyRequest().authenticated();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = this.clientRegistration();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github")
-                .clientId("7efb15ac1cdde34b0901")
-                .clientSecret("bccd7f002c96cae30645159d67d8d444bf16fb6c")
-                .build();
     }
 }
