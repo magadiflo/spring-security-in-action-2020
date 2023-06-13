9bcf0769b814bd0a1ddc8d8a575493d3512d0458
Martín <magadiflo@gmail.com>
2023-06-13 11:49:18 -0500

    Implementación de un servidor de recursos

---
+++ src/main/java/com/magadiflo/book/security/app/config/ResourceServerConfig.java
@@ -0,0 +1,10 @@
+package com.magadiflo.book.security.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@Configuration
+public class ResourceServerConfig {
+
+}
