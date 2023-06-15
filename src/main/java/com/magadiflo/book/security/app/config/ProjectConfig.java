6425ad8c3a46788c65d1d1847a467c95c8a3fc3e
Martín <magadiflo@gmail.com>
2023-06-15 16:53:01 -0500

    Habilitación de la seguridad de métodos globales (EnableGlobalMethodSecurity)

---
+++ src/main/java/com/magadiflo/book/security/app/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.magadiflo.book.security.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class ProjectConfig {
+}
