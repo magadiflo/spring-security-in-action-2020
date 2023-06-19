6da65f810399b6529de7e166eae9c1fbc67e5cdf
Martín <magadiflo@gmail.com>
2023-06-19 17:12:47 -0500

    Implementación del servidor de recursos - configuraciones inciales

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS workout (
+  id INT NOT NULL AUTO_INCREMENT,
+  user VARCHAR(45) NULL,
+  start DATETIME NULL,
+  end DATETIME NULL,
+  difficulty INT NULL,
+  PRIMARY KEY (id)
+);
